d5baedd049acd29a66ccce8a7f8a069782b220e7
Paul Heilmann <paul.heilmann@igs-goettingen.de>
2019-01-29 16:39:15 +0100

    Malte drive mode
    simple drive mode from Malte

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fahren_schlecht.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fahren_schlecht.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+    package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.teamcode.Tools.MotorStuff;
 public class Fahren_schlecht extends OpMode {
     HardwareChassisSun hwchs = null;
     MotorStuff mtstff = null;
+    public double speed;
     @Override
     public void init() {
         hwchs = new HardwareChassisSun(hardwareMap);
@@ -20,7 +21,28 @@ public class Fahren_schlecht extends OpMode {
 
     @Override
     public void loop() {
-        if()
+        speed = (double) (gamepad1.left_trigger);
+        if (gamepad1.dpad_up) {
+            mtstff.setAllMotors(speed, 0, -speed, 0);
+        }
+        if (gamepad1.dpad_down) {
+            mtstff.setAllMotors(-speed, 0, speed, 0);
+        }
 
+        if (gamepad1.dpad_right) {
+            mtstff.setAllMotors(0, speed, 0, -speed);
+        }
+        if (gamepad1.dpad_left) {
+            mtstff.setAllMotors(0, -speed, 0, speed);
+        }
+        if(gamepad1.dpad_right){
+            mtstff.setAllMotors(0,speed,0,speed);
+        }
+        if(gamepad1.x){
+            mtstff.setAllMotors(speed,speed,speed,speed);
+        }
+        if(gamepad1.b){
+            mtstff.setAllMotors(-speed,-speed,-speed,-speed);
+        }
     }
 }
