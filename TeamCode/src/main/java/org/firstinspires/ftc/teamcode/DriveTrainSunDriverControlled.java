5c4f80e9547c4e9735024da9c5de49ceb7ddff8e
Lena <lena.ellermann@igs-goettingen.de>
2019-02-28 17:32:11 +0100

    changed driver controlled - how to drive
    now with right trigger for speed and dpad for direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainSunDriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainSunDriverControlled.java
@@ -7,7 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.DriveModes.DriveHoverOptimized;
 import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassisSun;
 import org.firstinspires.ftc.teamcode.Tools.MotorStuff;
-@TeleOp (name = "Driver Controlled + arm")
+@TeleOp (name = "Driver Controlled with arm")
+
+//this dirve method is for chassis SunnyBoy
 public class DriveTrainSunDriverControlled extends OpMode {
     //declare an Object of Optimized Drive Mode we have decided to use this mode due to its practicality
     private DriveHoverOptimized driveOp;
@@ -34,11 +36,39 @@ public class DriveTrainSunDriverControlled extends OpMode {
 
     @Override
     public void loop() {
+       /*
         //this Part is for driving a bit like hovering
         //calculate the needed optimal speed for every motor in its direction
         driveOp.drive(gamepad1.left_stick_x,-gamepad1.left_stick_y, gamepad1.right_trigger);
         //drive with calculated values
         motstff.setAllMotors(driveOp.driveSpeedY, driveOp.driveSpeedX, driveOp.driveSpeedY, driveOp.driveSpeedX);
+        */
+       //we don't need to drive diagonal
+
+        //drive with dpad for direction and trigger for speed
+        if (gamepad1.dpad_up) { //drive forward
+            motstff.driveInOneDirection(gamepad1.right_trigger, gamepad1.right_trigger);
+        } else {
+            motstff.driveInOneDirection(0,0);
+        }
+
+        if (gamepad1.dpad_down) { //drive backwards
+            motstff.driveBack(gamepad1.right_trigger, gamepad1.right_trigger);
+        } else {
+            motstff.driveBack(0,0);
+        }
+
+        if (gamepad1.dpad_left) { //drive left
+            motstff.driveLeft(gamepad1.right_trigger,  gamepad1.right_trigger);
+        } else {
+            motstff.driveLeft(0,0);
+        }
+
+        if (gamepad1.dpad_right) { //drive right
+            motstff.driveRight(gamepad1.right_trigger,gamepad1.right_trigger);
+        } else {
+            motstff.driveRight(0,0);
+        }
 
         //to turn the robot if wanted
         motstff.turnWithGamepad(gamepad1.left_bumper,gamepad1.right_bumper,1);
@@ -46,15 +76,32 @@ public class DriveTrainSunDriverControlled extends OpMode {
         //controlling arm
         if (gamepad2.dpad_up) {
             motor_arm_lift.setPower(0.3); //arm ausfahren
-        } else if (gamepad2.dpad_down) {
-            motor_arm_lift.setPower(-0.3); //arm einfahren
-        } else if (gamepad2.a) {
-            motor_arm_tilt.setPower(0.2); //winkel arm nach oben
-            motor_arm_lift.setPower(-0.3); //arm parallel einfahren, add until feedback
-        } else if (gamepad2.b) {
-            motor_arm_tilt.setPower(-0.2); //winkel arm nach unten
-            motor_arm_lift.setPower(-0.3); //arm parallel einfahren, add until feedback
+        } else {
+            motor_arm_lift.setPower(0);
         }
+
+        if (gamepad2.dpad_down) {
+            motor_arm_lift.setPower(-0.3); //arm einfahren
+        } else {
+            motor_arm_lift.setPower(0);
+        }
+
+        if (gamepad2.a) {
+                motor_arm_tilt.setPower(0.1); //winkel arm nach oben
+                motor_arm_lift.setPower(-0.3); //arm parallel einfahren, add until feedback
+            } else {
+            motor_arm_tilt.setPower(0);
+            motor_arm_lift.setPower(0);
+        }
+
+        if (gamepad2.b) {
+            motor_arm_tilt.setPower(-0.1); //winkel arm nach unten
+            motor_arm_lift.setPower(-0.3); //arm parallel einfahren, add until feedback
+            } else {
+            motor_arm_tilt.setPower(0);
+            motor_arm_lift.setPower(0);
+            }
+
 
         //add collector etc.
     }
