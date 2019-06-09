b629f8ae52099952eae40a2a232e2dc0e5296898
Paul Heilmann <paulhenrik2508@gmail.com>
2019-06-09 14:55:00 +0200

    fixxed einfach fahren
    now the robot is actually able to stop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fahrklassen/WorkshopEinfachFahren.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fahrklassen/WorkshopEinfachFahren.java
@@ -15,8 +15,8 @@ public class WorkshopEinfachFahren extends OpMode {
 
     private DcMotor motor_driveLeft;
     private DcMotor motor_driveRight;
-    private DcMotor motor_addition_0;
-    private DcMotor motor_addition_1;
+    private DcMotor motor_addition_port2;
+    private DcMotor motor_addition_port3;
 
 
 
@@ -24,8 +24,8 @@ public class WorkshopEinfachFahren extends OpMode {
     public void init() {
         motor_driveLeft = hardwareMap.dcMotor.get("motor_hub1_port0");
         motor_driveRight = hardwareMap.dcMotor.get("motor_hub1_port1");
-        motor_addition_0 = hardwareMap.dcMotor.get("motor_hub1_port2");
-        motor_addition_1 = hardwareMap.dcMotor.get("motor_hub1_port3");
+        motor_addition_port2 = hardwareMap.dcMotor.get("motor_hub1_port2");
+        motor_addition_port3 = hardwareMap.dcMotor.get("motor_hub1_port3");
 
         motor_driveLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motor_driveRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -48,8 +48,13 @@ public class WorkshopEinfachFahren extends OpMode {
             motor_driveRight.setPower(-0.5);
             motor_driveLeft.setPower(0.5);
         }
-        motor_addition_0.setPower(gamepad1.left_stick_y);
-        motor_addition_1.setPower(gamepad1.right_stick_y);
+        if(!gamepad1.dpad_right && !gamepad1.dpad_up && ! gamepad1.dpad_left) {
+            //TurnLeft
+            motor_driveRight.setPower(0);
+            motor_driveLeft.setPower(0);
+        }
+        motor_addition_port2.setPower(gamepad1.left_stick_y);
+        motor_addition_port3.setPower(gamepad1.right_stick_y);
 
 
     }
